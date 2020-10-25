7cdc837dae2103d477e0df21565e11d19cf0a45f
Oleg <artoleg99@gmail.com>
2020-10-25 16:37:11 +0300

    homework 13

--- Yoga_project/js/script.js
+++ Yoga_project/js/script.js
@@ -139,5 +139,65 @@ window.addEventListener('DOMContentLoaded', function() {
     const item = new Options(300, 350, "red", 14, "center");
 
     item.createDiv();
-}); 
 
+    // Form
+
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'         
+    };
+
+    let form = document.querySelector('.main-form'),
+        formBottom = document.getElementById('form'),
+        input = document.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    function sendForm(elem) {            
+        elem.addEventListener('submit', function(event) {
+            event.preventDefault();
+            elem.appendChild(statusMessage);
+            let formData = new FormData(elem);
+
+            function postData(data) {
+
+                return new Promise(function(resolve,reject) {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', '../server.php');
+
+                    request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+                    request.onreadystatechange = function() {
+                        if (request.readyState < 4) {
+                            resolve();                                                
+                        } else if(request.readyState === 4 && request.status == 200) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    };
+                    request.send(data);
+                });
+            } //End of postData
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }                
+            }
+
+            postData(formData)                                                  // Получение статуса через promise
+                .then(()=> statusMessage.innerHTML = message.loading)
+                .then(()=> statusMessage.innerHTML = message.success)
+                .catch(()=> statusMessage.innerHTML = message.failure)
+                .then(clearInput)
+                .then(setTimeout((()=> statusMessage.innerHTML = ''), 10000)); // Автоматическое очищение статуса через 10 сек
+        });
+    }
+
+    sendForm(form);
+    sendForm(formBottom);
+
+}); 
